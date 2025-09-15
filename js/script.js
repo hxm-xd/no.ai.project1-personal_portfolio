be2fd9d3d2018df4f9b3838351d8f3f080a77f9e
Hamood Thariq <143422757+hxm-xd@users.noreply.github.com>
2025-09-15 21:07:58 +0530

    Added a picture slideshow to the hero section and made some minor changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+particlesJS("particles-js", {
+ particles: {
+  number: { value: 250 },
+   size: { value: 2 },
+   move: { speed: 0.6 },
+   color: { value: "#8a2be2"},
+  line_linked: {
+    enable: true,
+   distance: 150,
+   color: "#8a2be2",
+   opacity: 0.8,
+   width: 0.5,
+    }, 
+   },
+});
+
 
 document.addEventListener("DOMContentLoaded", () => {
       const header = document.querySelector("#navbar");
@@ -8,23 +24,25 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
           header.classList.remove("shrink", "white-nav");
         }
-      });
-
-      // Initialize particles.js
-      particlesJS("particles-js", {
-        particles: {
-          number: { value: 250 },
-          size: { value: 2 },
-          move: { speed: 0.6 },
-          color: { value: "#8a2be2"},
-          line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#8a2be2",
-            opacity: 0.8,
-            width: 0.5,
-          },
-        },
-      });
+  
     });
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+  const images = ["../images/Pera_me.jpg", "../images/Me_wrkshp.jpg", "../images/Me_sprit.jpg"
+    ,"../images/Me_pera2.jpg", "../images/Me_wrkshp.jpg"
+  ];
+
+  let index = 0;
+  const slideshow = document.getElementById("photos-me");
+
+  setInterval(() => {
+    slideshow.style.opacity = 0;
+    setTimeout(() => {
+      index = (index + 1) % images.length;
+      slideshow.src = images[index];
+      slideshow.style.opacity = 1;
+    }, 500);
+  }, 3000);
+
+});
