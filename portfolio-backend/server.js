7c1e8f47053db3486a608e643020e98fc555e755
Hamood Thariq <143422757+hxm-xd@users.noreply.github.com>
2025-09-16 21:22:27 +0530

    Successfully implemented a REST Api to handle projects without having to hardcode it into the frontend

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -6,10 +6,27 @@ const port = 5050;
 app.use(express.json());
 app.use(cors());
 
+
+//Mock data
 const projects = [
-  { id: 1, title: "Landslide Detection and Monitoring System", description: "My frontend portfolio", link: "https://hamood-portfolio.com"},
-  { id: 2, title: "Smart City Project", description: "Miniature Kandy project", link: "https://hamood-portfolio.com"}
+    { 
+        id: 1,
+        title: "Landslide Detection and Monitoring System",
+        timeframe: "July 2025 - present",
+        description: `Developing a real-time landslide detection system using IoT,
+              robotics, and AI to enhance disaster preparedness in Sri Lanka.
+              The system monitors soil moisture, ground vibrations, and
+              rainfall, sending instant mobile alerts to residents and
+              authorities when risk thresholds are reached. Using AI and machine
+              learning to improve predictions, the project aligns with NBRO
+              standards to ensure effective deployment. Aims to provide a
+              scalable, cost-effective solution to reduce landslide risks.`,
+        link: "https://hamood-portfolio.com",
+        toolList: ["ESP32", "Firebase", "Arduino IDE", "Blynk"]
+    }
 ];
+
+
 
 app.get("/api/projects",(req, res) =>{
     res.json(projects);
